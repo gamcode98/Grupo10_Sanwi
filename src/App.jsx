1e16aefe5aed6dc395c7fc56e7d2c69ce46a1176
Gabriel <gamcode98@gmail.com>
2023-12-17 14:56:22 -0300

    add product detail2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Dashboard } from './routes/Dashboard/Dashboard'
 import { Users } from './routes/Users/Users'
 import { Products } from './routes/Products/Products'
 import { ProductForm } from './routes/ProductForm/ProductForm'
+import { ProductDetail } from './routes/ProductDetail/ProductDetail'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/users' element={<Users />} />
         <Route path='/products' element={<Products />} />
         <Route path='/products/edit/:id' element={<ProductForm />} />
+        <Route path='/products/detail/:id' element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   )
